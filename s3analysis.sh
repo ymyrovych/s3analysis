24d6ac8dd8d9e27e9f3fead4bf810557494fedc3
ymyrovych <ymyrov@gmail.com>
2018-08-16 13:09:36 +0300

    Added message if no S3 buckets available in configured account

--- s3analysis.sh
+++ s3analysis.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 aws s3 ls | cut -d ' ' -f 3 > bucket_list.tmp
 bucket_num=`wc -l < bucket_list.tmp`
+ if [[ $bucket_num -eq 0 ]]; then
+    echo "No S3 buckets available in configured AWS CLI account."
+    exit 1
+ fi
    clear
    echo "   ***   S3 BUCKETS SUMMARY    ***"
    echo "__________________________________"
    echo
  while read bucket;
  do
-    if [[ $bucket_num -gt 1 ]]; then
+  if [[ $bucket_num -gt 1 ]]; then
    creation_date=`aws s3api list-buckets --output table | grep $bucket | grep "|  $bucket" | cut -c 5,6,7,8,9,10,11,12,13,14 | head -1`
-    else
+  else
    creation_date=`aws s3 ls | cut -d ' ' -f1`
-    fi
+  fi
    number_of_files=`aws s3 ls $bucket --recursive | grep -v '\/$' | wc -l`
    size_of_files=`aws s3 ls $bucket --recursive --summarize --human-readable | grep "Total Size:" | cut -d " " -f6,7`
-
    echo "Bucket name: " $bucket
    echo "Creation date: " $creation_date
    echo "Number of files: " $number_of_files
