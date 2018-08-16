cf92890d8c843aa889f9b91c37166aaa1a39c1ce
ymyrovych <ymyrov@gmail.com>
2018-08-16 12:52:18 +0300

    Bug fix and refactoring

--- s3analysis.sh
+++ s3analysis.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
 aws s3 ls | cut -d ' ' -f 3 > bucket_list.tmp
+bucket_num=`wc -l < bucket_list.tmp`
    clear
    echo "   ***   S3 BUCKETS SUMMARY    ***"
    echo "__________________________________"
    echo
-while read bucket;
+ while read bucket;
  do
+    if [[ $bucket_num -gt 1 ]]; then
+   creation_date=`aws s3api list-buckets --output table | grep $bucket | grep "|  $bucket" | cut -c 5,6,7,8,9,10,11,12,13,14 | head -1`
+    else
+   creation_date=`aws s3 ls | cut -d ' ' -f1`
+    fi
+   number_of_files=`aws s3 ls $bucket --recursive | grep -v '\/$' | wc -l`
+   size_of_files=`aws s3 ls $bucket --recursive --summarize --human-readable | grep "Total Size:" | cut -d " " -f6,7`
+
    echo "Bucket name: " $bucket
-   echo "Creation date: " `aws s3api list-buckets --output table | grep $bucket | grep "|  $bucket" | cut -c 5,6,7,8,9,10,11,12,13,14 | head -1`
-   echo "Number of files: " `aws s3 ls $bucket --recursive | grep -v '\/$' | wc -l`
-   echo "Total size of files: " `aws s3 ls $bucket --recursive --summarize --human-readable | grep "Total Size:" | cut -d " " -f6,7`
+   echo "Creation date: " $creation_date
+   echo "Number of files: " $number_of_files
+   echo "Total size of files: " $size_of_files
    echo "__________________________________"
    echo
  done < bucket_list.tmp
